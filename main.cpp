1660873a244c3241b94c1b6ad910230700714fd9
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2023-05-22 17:41:21 -0600

    feat(graph): added graph utils

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "utils.h"
+#include "read.h"
+#include "graph.h"
 
 using namespace std;
 
@@ -7,6 +8,26 @@ int main(int argc, char* argv[]) {
 
     IRegex res = read_regex();
     print_readed_regex(res);
-    
-	return 0;
+
+	WListGraph<char, int> wmg;
+	
+	wmg.addEdge('A', 'B', 1);
+	wmg.addEdge('A', 'A', 4);
+	wmg.addEdge('B', 'C', 1);
+	wmg.addEdge('B', 'D', 1);
+	wmg.addEdge('C', 'E', 1);
+	wmg.addEdge('D', 'C', 1);
+	wmg.addEdge('E', 'D', 1);
+	
+	std::cout << wmg.toString();
+	
+	std::map<char, int> wedges = wmg.getConnectionFrom('D');
+	std::map<char, int>::iterator witr;
+	
+	std::cout << "B ->\t";
+	for (witr = wedges.begin(); witr != wedges.end(); witr++) {
+		std::cout << "(" << witr->first << "," << witr->second << ")\t";
+	}
+
+    return 0;
 }
---
+++ parse.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "utils.h"
+
+using namespace std;
+
+void parse_regex(IRegex regex) {
+      
+};
