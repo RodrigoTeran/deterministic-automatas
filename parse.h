15c44da35768c7e59e907217a393607a7c7aca66
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-05-27 23:19:56 -0600

    feat: parse try2

--- parse.h
+++ parse.h
@@ -3,29 +3,32 @@
 
 using namespace std;
 
-string parse_regex(const IRegex& regex) {
+string parse_regex(const IRegex& res) {
     string parsed_string;
 
-    for (int i = 0; i < regex.size_alphabet; i++) {
-        char current_char = regex.alphabet[i];
+    for (int i = 0; i < res.size_alphabet; i++) {
+        string current_char = res.alphabet[i];
+        string current_char1 = res.alphabet[i+1];
+        char c_char = current_char[0];
+        char c_char1 = current_char1[0];
 
-        if (isalpha(current_char) && i + 1 < regex.size_alphabet && isalpha(regex.alphabet[i + 1])) {
+        if (isalpha(c_char) && i + 1 < res.size_alphabet && isalpha(c_char1)) {
             parsed_string += current_char;
             parsed_string += '.';
         } 
         
-        else if ((current_char == '*' || current_char == '+') && i + 1 < regex.size_alphabet && isalpha(regex.alphabet[i + 1])) {
-            parsed_string += current_char;
+        else if ((c_char == '*' || c_char == '+') && i + 1 < res.size_alphabet && isalpha(c_char1)) {
+            parsed_string += c_char;
             parsed_string += '.';
         } 
         
-        else if ((current_char == '*' || current_char == '+') && i + 1 < regex.size_alphabet && regex.alphabet[i + 1] == '(') {
-            parsed_string += current_char;
+        else if ((c_char == '*' || c_char == '+') && i + 1 < res.size_alphabet && c_char1 == '(') {
+            parsed_string += c_char;
             parsed_string += '.';
         } 
         
         else {
-            parsed_string += current_char;
+            parsed_string += c_char;
         }
     }
 
