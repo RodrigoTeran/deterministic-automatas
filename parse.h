0e7cda0051eac60aacec5e16b217103515d49e17
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-05-27 22:33:11 -0600

    feat: first try parse_regrex

--- parse.h
+++ parse.h
@@ -3,6 +3,31 @@
 
 using namespace std;
 
-void parse_regex(IRegex regex) {
-      
-};
+string parse_regex(const IRegex& regex) {
+    string parsed_string;
+
+    for (int i = 0; i < regex.size_alphabet; i++) {
+        char current_char = regex.alphabet[i];
+
+        if (isalpha(current_char) && i + 1 < regex.size_alphabet && isalpha(regex.alphabet[i + 1])) {
+            parsed_string += current_char;
+            parsed_string += '.';
+        } 
+        
+        else if ((current_char == '*' || current_char == '+') && i + 1 < regex.size_alphabet && isalpha(regex.alphabet[i + 1])) {
+            parsed_string += current_char;
+            parsed_string += '.';
+        } 
+        
+        else if ((current_char == '*' || current_char == '+') && i + 1 < regex.size_alphabet && regex.alphabet[i + 1] == '(') {
+            parsed_string += current_char;
+            parsed_string += '.';
+        } 
+        
+        else {
+            parsed_string += current_char;
+        }
+    }
+
+    return parsed_string;
+}
